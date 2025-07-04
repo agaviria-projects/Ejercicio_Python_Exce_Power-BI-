f9cbffe99343c2248c1ecb3c77768126fcc609e4
Alejandro <agaviria1408@gmail.com>
2025-07-04 12:15:27 -0500

    Se crean comentarios

--- ventas.py
+++ ventas.py
@@ -4,8 +4,12 @@ import pandas as pd
 archivo='ventas_dia2.xlsx'
 df=pd.read_excel(archivo,sheet_name= 'Sheet1')
 
+#Generar un ID
 df['ID']=range(1,len(df)+1)
+
+#Reordenar columnas
 df=df[["ID","FechaVenta","Zona","Producto","PrecioVenta","Costo"]]
+
 
 
 print(df)
